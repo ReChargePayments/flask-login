fc895aa4eed63846b7b6837f846ed29b25a4509a
Lke Otterblad <luke@rechargeapps.com>
2020-01-29 11:43:32 -0600

    add samesite to config and set_cookie function

--- flask_login/login_manager.py
+++ flask_login/login_manager.py
@@ -14,8 +14,9 @@ from flask import (_request_ctx_stack, abort, current_app, flash, redirect,
 
 from ._compat import text_type
 from .config import (COOKIE_NAME, COOKIE_DURATION, COOKIE_SECURE,
-                     COOKIE_HTTPONLY, LOGIN_MESSAGE, LOGIN_MESSAGE_CATEGORY,
-                     REFRESH_MESSAGE, REFRESH_MESSAGE_CATEGORY, ID_ATTRIBUTE,
+                     COOKIE_HTTPONLY, COOKIE_SAMESITE, LOGIN_MESSAGE,
+                     LOGIN_MESSAGE_CATEGORY, REFRESH_MESSAGE,
+                     REFRESH_MESSAGE_CATEGORY, ID_ATTRIBUTE,
                      AUTH_HEADER_NAME, SESSION_KEYS)
 from .mixins import AnonymousUserMixin
 from .signals import (user_loaded_from_cookie, user_loaded_from_header,
@@ -397,6 +398,7 @@ class LoginManager(object):
 
         secure = config.get('REMEMBER_COOKIE_SECURE', COOKIE_SECURE)
         httponly = config.get('REMEMBER_COOKIE_HTTPONLY', COOKIE_HTTPONLY)
+        samesite = config.get('REMEMBER_COOKIE_SAMESITE', COOKIE_SAMESITE)
 
         # prepare data
         data = encode_cookie(text_type(session['user_id']))
@@ -415,7 +417,8 @@ class LoginManager(object):
                             domain=domain,
                             path=path,
                             secure=secure,
-                            httponly=httponly)
+                            httponly=httponly,
+                            samesite=samesite)
 
     def _clear_cookie(self, response):
         config = current_app.config
